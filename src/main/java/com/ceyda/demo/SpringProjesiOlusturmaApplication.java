7aa0efd833fd60f84e83b8d5e68894b533f2bf03
Ceyda Kaba <ceydakaba@gmail.com>
2025-08-25 13:06:28 +0300

    Initial commit

---
+++ src/main/java/com/ceyda/demo/SpringProjesiOlusturmaApplication.java
@@ -0,0 +1,13 @@
+package com.ceyda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjesiOlusturmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjesiOlusturmaApplication.class, args);
+	}
+
+}
